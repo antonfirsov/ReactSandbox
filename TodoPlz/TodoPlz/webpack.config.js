27f014a196669d58eea0aeb4bb826a632b449964
Anton Firszov <anton.firszov@sapagroup.com>
2017-11-11 02:59:53 +0100

    configured npm stuff

---
+++ TodoPlz/TodoPlz/webpack.config.js
@@ -0,0 +1,31 @@
+﻿var path = require('path');
+
+module.exports = {
+    entry: {
+        site: [
+            './scripts/Main.tsx'
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'wwwroot/dist/')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/
+                //,use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+
+    devtool: "source-map"
+};
