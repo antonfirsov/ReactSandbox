dfb47ce06715428be042e2dd563fe20926f36ea0
Anton Firszov <anton.firszov@sapagroup.com>
2017-11-11 02:30:15 +0100

    yet another basic Mvc project

---
+++ NaMostma/NaMosma/scripts/components/Hello.test.tsx
@@ -0,0 +1,22 @@
+﻿import * as React from 'react';
+import TestUtils from 'react-addons-test-utils';
+
+import * as enzyme from 'enzyme';
+import jasmineEnzyme from 'jasmine-enzyme';
+import Hello from './Hello';
+
+describe("Rezsi", () => {
+
+        beforeEach(() => {
+            jasmineEnzyme();
+        });
+
+        it('is always true', () => {
+                expect(true);
+            });
+    });
+
+//it('renders the correct text when no enthusiasm level is given', () => {
+//    const hello = enzyme.shallow(<Hello name='Daniel' />);
+//    expect(hello.find(".greeting").text()).toEqual('Hello Daniel!')
+//});
